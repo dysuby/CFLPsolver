f3b8b87da85286608137066880f28365aef9927c
Veg_chic <h_foryou@163.com>
2018-12-21 09:48:36 +0800

    修复下标错误的bug

--- SA.py
+++ SA.py
@@ -42,10 +42,13 @@ class SA:
         return Solution(cost, is_opened, assigned, left)
 
     def swap_facility(self, solution):
-        customer = [[x, y] for x in range(self.customer_num) for y in range(self.customer_num)]
+        customer = [[x, y] for x in range(
+            self.customer_num) for y in range(self.customer_num)]
         i, j = random.sample(customer, 1)[0]
         while solution.left[solution.assigned[j]] + self.demand[j] < self.demand[i] or solution.left[solution.assigned[i]] + self.demand[i] < self.demand[j]:
             customer.remove([i, j])
+            if not customer:
+                return solution
             i, j = random.sample(customer, 1)[0]
 
         new_assigned = solution.assigned.copy()
@@ -64,27 +67,58 @@ class SA:
 
         return Solution(new_cost, solution.is_opened, new_assigned, new_left)
 
-    def open_facility(self, solution):
-        non_open = np.where(solution.is_opened == 0)
-        to_open = random.sample(non_open, 1)
-        new_is_opened = solution.is_opened.copy()
-        new_is_opened[to_open] = 1
+    def move_facility(self, solution):
+        target, = random.sample(range(self.facility_num), 1)
 
-        new_cost = solution.cost + self.opening_cost[to_open]
+        new_is_opened = solution.is_opened.copy()
+        new_cost = solution.cost
+        if not solution.is_opened[target]:
+            new_is_opened[target] = 1
+            new_cost += self.opening_cost[target]
         new_left = solution.left.copy()
         new_assigned = solution.assigned.copy()
-        for i in range(self.customer_num):
-            if self.cost[to_open, i] < self.cost[solution.assigned[i], i] and new_left[to_open] > self.demand[i]:
-                new_cost += self.cost[to_open, i] - self.cost[solution.assigned[i], i]
-                new_assigned[i] = to_open
-                new_left[to_open] -= self.demand[i]
 
-        return Solution(new_cost, new_is_opened, new_assigned, new_left)                
+        indices = list(range(self.customer_num))
+        random.shuffle(indices)
+        for i in indices:
+            if new_left[target] >= self.demand[i]:
+                new_cost += self.cost[target, i] - \
+                    self.cost[solution.assigned[i], i]
+                new_assigned[i] = target
+                new_left[target] -= self.demand[i]
+                new_left[solution.assigned[i]] += self.demand[i]
+
+        return Solution(new_cost, new_is_opened, new_assigned, new_left)
+
+    def move_customer(self, solution):
+        cust, = random.sample(range(self.customer_num), 1)
+        can_choose = list(range(self.facility_num))
+        fac, = random.sample(can_choose, 1)
+        while solution.assigned[cust] == fac and solution.left[fac] < self.demand[cust]:
+            can_choose.remove(fac)
+            if not can_choose:
+                return solution
+            fac, = random.sample(can_choose, 1)
+        new_assigned = solution.assigned.copy()
+        new_assigned[cust] = fac
+
+        new_cost = solution.cost
+        new_cost += self.cost[fac, cust] - self.cost[solution.assigned[cust], cust]
+
+        new_is_opened = solution.is_opened.copy()
+        if not new_is_opened[fac]:
+            new_is_opened[fac] = 1
+            new_cost += self.opening_cost[fac]
+
+        new_left = solution.left.copy()
+        new_left[solution.assigned[cust]] += self.demand[cust]
+        new_left[fac] -= self.demand[cust]
+
+        return Solution(new_cost, new_is_opened, new_assigned, new_left)
 
     def localsearch(self, solution):
-        neighbors = [self.swap_facility(solution)]
-        if np.count_nonzero(solution.is_opened == 0):
-            neighbors.append(self.open_facility(solution))
+        neighbors = [self.move_customer(
+            solution), self.swap_facility(solution)]
         return min(neighbors, key=lambda s: s.cost)
 
     def run(self, T, tmin, ntimes, T_ratio):
@@ -122,7 +156,7 @@ if __name__ == '__main__':
         print('instances: {}'.format(i))
         st = time()
         sa = SA('p{}'.format(i))
-        tc, io, ass = sa.run(100, 5, 5000, 0.97)
+        tc, io, ass = sa.run(100, 1, 3000, 0.95)
         cost_time.append(time() - st)
         result.append(tc)
         write_details('sa', i, tc, io, ass)
